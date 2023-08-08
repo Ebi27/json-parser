75367e93e668b24ab98303e14baa060186a9c144
unknown <kepmiogokimiebi@gmail.com>
2023-08-08 16:27:02 +0200

    Implement Lexer class for tokenizing input text.

--- cc_json/lexer.py
+++ cc_json/lexer.py
@@ -1,79 +1,34 @@
-import ply.lex as lex
-from lexer_tokens import tokens
+from lexer_tokens import Token_Definitions
+import re
 
 
 class GetToken:
-    def __init__(self, token_type, token_value):
+    def __init__(self, token_type, token_value, curr_position):
         self.type = token_type
+        self.position = curr_position
         self.value = token_value
 
 
-class SourceReader:
-    def __init__(self, source_code):
-        self.source_code = source_code
-
-    def read_source(self):
-        #  read the input source code from file
-        with open(self.source_code, 'r') as file:
-            return file.read()
-
-
 class Lexer:
-    def __init__(self, input_string):
-        self.input_string = input_string
-        self.current_index = 0
-        self.lexer = lex.lex(module=self)  # To initialize the lexer
-
-    # Regular expression rules for simple tokens
-    t_LPAREN = r'\('
-    t_RPAREN = r'\)'
-    t_LBRACE = r'\['
-    t_RBRACE = r'\]'
-    t_BLOCKSTART = r'\{'
-    t_BLOCKEND = r'\}'
-    t_COLON = r':'
-    t_DOUBLE_QUOTE = r'"'
-    t_WS = r'\s+'  # Ignore white spaces
-
-    # Regular expression rule for NUMBER and FLOAT
-    @staticmethod
-    def t_NUMBER(t):
-        r'\d+'
-        t.value = int(t.value)
-        return t
-
-    @staticmethod
-    def t_FLOAT(t):
-        r'(\d*\.\d+)|(\d+\.\d*)'
-        t.value = float(t.value)
-        return t
-
-    # Regular expression rule for STRING
-    @staticmethod
-    def t_STRING(t):
-        r'"([^"\\]|\\.)*"'  # Matches double-quoted strings, handling escape sequences
-        t.value = t.value[1:-1]  # Remove the double quotes from the value
-        return t
-
-    # Regular expression rule for BOOLEAN
-    @staticmethod
-    def t_BOOLEAN(t):
-        r'true|false'
-        t.value = (t.value == 'true')  # Convert "true" to True, "false" to False
-        return t
-
-    # Error handling rule
-    @staticmethod
-    def t_error(t):
-        print("Illegal character '%s'" % t.value[0])
-        t.lexer.skip(1)
-
-    # Build the lexer
-    def build(self, **kwargs):
-        self.lexer = lex.lex(module=self, **kwargs)
+    def __init__(self, input_text):
+        self.text = input_text
+        self.position = 0
+        self.tokens = []
 
     # Tokenize the input string
-    def getToken(self):
-        self.lexer.input(self.input_string)
-        for token in self.lexer:
-            yield GetToken(token.type, token.value)
+    def tokenize(self):
+        while self.position < len(self.text):
+            self.get_next_token()
+
+    def get_next_token(self):
+        for token_type, rgx in Token_Definitions:
+            pattern = re.compile(rgx)
+            match = pattern.match(self.text, pos=self.position)
+            if match:
+                value = match.group()
+                self.position += len(value)
+                token = GetToken(token_type, value, self.position)
+                self.tokens.append(token)
+                return
+        raise ValueError("Unrecognized token")
+
