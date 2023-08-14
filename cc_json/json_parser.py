891d41c01988de7f6910824ccc21af73efed78b5
unknown <kepmiogokimiebi@gmail.com>
2023-08-14 18:50:00 +0200

    Add Command-Line Argument Handling to the Json parser.

--- cc_json/json_parser.py
+++ cc_json/json_parser.py
@@ -1,46 +1,44 @@
+import argparse
 import os
-from dotenv import load_dotenv
-
-
-# To load environment variables from the .env file
-load_dotenv()
-python_path = os.getenv("PYTHONPATH")
-other_variable = os.getenv("OTHER_VARIABLE")
-
-# To use the variables in the script
-print(f"PYTHONPATH: {python_path}")
-print(f"OTHER_VARIABLE: {other_variable}")
+from lexer import Lexer
+from parser import Parser
 
 
 class JSONParser:
     def __init__(self):
-        pass
-
-    # The tokenization of the lexer input_string will be done here.
-    def lex(self, input_string):
-        pass
-
-    # The logic that checks if a parsed token is a valid JSON object will be done here.
-    def parse(self, tokens):
-        pass
+        self.lexer = Lexer()
+        self.parser = Parser()
 
     def parse_json_file(self, file_path):
-        with open(file_path, 'r') as file:
-            input_string = file.read()
-
-        tokens = self.lex(input_string)
-        exit_code = 0 if self.parse(tokens) else 1
-        return exit_code
+        try:
+            with open(file_path, 'r') as file:
+                input_text = file.read()
+                tokens = self.lexer.tokenize(input_text)
+                ast = self.parser.parse(tokens)
+                return ast
+        except FileNotFoundError:
+            print(f"Error: File not found - {file_path}")
+            return None
 
 
 if __name__ == '__main__':
     import sys
-
-    if len(sys.argv) != 2:
-        print("Usage: python json_parser.py <file_path>")
-        sys.exit(1)
-
-    file_path = sys.argv[1]
     parser = JSONParser()
-    exit_code = parser.parse_json_file(file_path)
-    sys.exit(exit_code)
+
+    # Creating argument parser
+    arg_parser = argparse.ArgumentParser(description = 'JSON Parser')
+    arg_parser.add_argument("file_path, help = 'Path to the Json file to parse'")
+
+    # Parse command line argument
+    args = arg_parser.parse_args()
+
+    # Make an attempt to parse the specified Json file
+    ast = parser.parse_json_file(args.file_path)
+
+    # check if parsing succeeded and exit with appropriate code as specified in challenge
+    if ast:
+        sys.exit(0)  # exit with code 0 for success
+    else:
+        sys.exit(1)  # exit with code 1 for failure
+
+
